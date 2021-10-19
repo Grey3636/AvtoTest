be8535687aa84e46800b5c91e32e22949d074968
Grey3636 <92727230+Grey3636@users.noreply.github.com>
2021-10-19 20:38:42 +0700

    Настройки проекта

---
+++ sandbox/src/main/java/rus/stqa/pft/sandbox/Hello.java
@@ -0,0 +1,16 @@
+package rus.stqa.pft.sandbox;
+
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+    System.out.println(2+2);
+    System.out.println(22*2);
+    System.out.println(2/2);
+    System.out.println(22-2);
+
+    System.out.println(1.0/2);
+
+    System.out.println("1"+"11"+"c");
+  }
+}
